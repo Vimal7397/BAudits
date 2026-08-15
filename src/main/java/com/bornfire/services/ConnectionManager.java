3769c5bd5a4d2f6e68ca1713c360149a99653799
Vimal7397 <vimal06102003@gmail.com>
2026-08-15 11:52:45 +0530

    after jasper changes

--- src/main/java/com/bornfire/services/ConnectionManager.java
+++ src/main/java/com/bornfire/services/ConnectionManager.java
@@ -14,7 +14,7 @@ public class ConnectionManager {
 				
 		        //conn = DriverManager.getConnection("jdbc:oracle:thin:@43.240.66.155:1521:ORCL","BTM_TEST","btmtest");
 				//conn = DriverManager.getConnection("jdbc:oracle:thin:@43.240.66.158:1600:BTM","btm","btm");
-				conn = DriverManager.getConnection("jdbc:oracle:thin:@103.224.241.244:1521/orcl","btm","btm");
+				conn = DriverManager.getConnection("jdbc:oracle:thin:@103.224.241.244:1521/orcl","BTM","btm");
 	    
 				//conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.1.99:1521:orcl","btm","btm");
                 //conn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.3.162:1521:orcl","btm","btm1");
